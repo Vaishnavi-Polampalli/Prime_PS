0ba340c9e86687fcb108d0a19cc3352086f29394
Vaishnavi Polampalli <polampvi@mail.uc.edu>
2024-08-08 00:01:31 -0400

    first commit

---
+++ prime/src/main/java/com/df/prime/WdAvailabilityApplication.java
@@ -0,0 +1,13 @@
+package com.df.prime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdAvailabilityApplication.class, args);
+	}
+
+}
---
+++ prime/src/test/java/com/df/prime/WdAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df.prime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdAvailabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
